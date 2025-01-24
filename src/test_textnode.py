e16c63f46e29b8c16ef3cc3828a1e166a29aa9cf
SparkingSquirrel <sparkingsquirrel@gmail.com>
2025-01-24 08:55:44 -0600

    test inequalities for text nodes

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,20 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
+    def test_different_text(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a different node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_different_text_type(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_different_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, 'a url')
+        node2 = TextNode("This is a text node", TextType.BOLD, 'a different url')
+        self.assertNotEqual(node, node2)
 
 if __name__ == "__main__":
     unittest.main()
