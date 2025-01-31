4456de0ff8895ea791da9acdc4a591d4909984df
SparkingSquirrel <sparkingsquirrel@gmail.com>
2025-01-31 09:49:38 -0600

    recursive pages

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,6 +1,16 @@
 import os
 from block_markdown import markdown_to_html_node
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        source_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename.replace('.md', '.html'))
+
+        if os.path.isfile(source_path):
+            generate_page(source_path, template_path, dest_path)
+        if os.path.isdir(source_path):
+            generate_pages_recursive(source_path, template_path, dest_path)
+
 
 def generate_page(from_path, template_path, dest_path):
     print(f" * {from_path} {template_path} -> {dest_path}")
